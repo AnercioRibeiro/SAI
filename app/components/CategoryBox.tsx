34cf00c6a1e08b75547895daeace218f82cc1039
AnercioRibeiro <anercio.ribeiro@outlook.com>
2023-09-18 22:31:41 +0100

    Select, CategoryBox and on url

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,4 +1,7 @@
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 import { IconType } from "react-icons";
+import qs from "query-string";
 
 interface CategoryBoxProps{
     icon: IconType;
@@ -11,8 +14,35 @@ icon: Icon,
 label,
 selected
 }) => {
+    const router = useRouter();
+    const params = useSearchParams();
+
+    const handleClick = useCallback(()=>{
+        let currentQuery = {};
+
+        if (params) {
+            currentQuery = qs.parse(params.toString());
+        }
+        const updatedQuery: any = {
+            ...currentQuery,
+            category: label
+        }
+
+        if (params?.get('category') === label) {
+            delete updatedQuery.category;
+        }
+
+        const url = qs.stringifyUrl({
+            url: '/',
+            query: updatedQuery
+        }, {skipNull: true});
+
+        router.push(url);
+      
+    }, [label, params, router])
     return ( 
-        <div 
+        <div
+            onClick={handleClick}
             className={`
                        flex flex-col 
                        items-center
@@ -26,6 +56,9 @@ selected
                        ${selected? 'text-neutral-800' : 'text-neutral-500'}
                        `}>
                         <Icon size={26}/>
+                        <div className="font-medium text-sm">
+                            {label}
+                        </div>
 
         </div>
      );
