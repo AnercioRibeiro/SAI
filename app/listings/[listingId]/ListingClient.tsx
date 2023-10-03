e024ac2a00131c568b0233bf250ffce6537389c9
Anercio Ribeiro <anercio.ribeiro@outlook.com>
2023-10-03 16:59:38 +0100

    Add more styling

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -35,6 +35,11 @@ currentUser
                                    id={listing.id}
                                    currentUser={currentUser}
                                     />
+                            <div className="grid grid-cols-1 md:grid-cols-7
+                                            md:gap-10 mt-6">
+
+                            </div>
+
                     </div>
                 </div>
             </Container>
